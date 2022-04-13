5c98696f49baf84d6b461f091f02f5f21bc8b4b6
Nikhita Rao <nikhitar@usc.edu>
2022-04-12 20:22:48 -0700

    added folder 12

---
+++ project-3/script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+    
+//$('.coolflower').click(function(){
+//     $('p').toggle();
+// });
+    
+//('.keyboard').click(function () {
+//        $('.infopanel').toggle(2000);     
+ //       $(this).toggleClass("bigger");
+ //   });
+    
+    $(function () {
+        $(".coolflower").draggable();
+    });
+  });
+    
