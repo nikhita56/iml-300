7e4e4cb6859f309bb4090ac45455f20c780ee5ac
Nikhita Rao <nikhitar@usc.edu>
2022-04-26 12:49:31 -0700

    idek

--- project-3/script.js
+++ project-3/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-
+   
+ $(function () {
+        $("#my-special-div").draggable();
   $(".girl").slideDown('slow');
 $(".girl2").slideDown('slow');
  
@@ -9,10 +11,20 @@ $(".girl2").slideDown('slow');
     
  
  $(".letherlivepg2").fadeIn('slow'); 
-    
+ $("button").delay(1000).fadeIn('slow');
 $(".containertoptext").slideDown('slow'); 
     $(".containertoptext2").slideDown('slow'); 
-       $(".topimage").fadeIn('slow');
-    $(".containerbottomtext").fadeIn('slow');
-    
+       $(".topimage").delay( 800 ).fadeIn('slow');
+    $(".containerbottomtext").slideDown('slow');
+    $(".pg2topimage").delay( 800 ).fadeIn('slow');
+    $(".pg2bottomtext").slideDown('slow');
+    $(".pg2toptext").slideDown('slow');
+       $(".pg2toptext2").slideDown('slow');
+    $(".pg3toptext").slideDown('slow');
+    $(".pg3toptext2").slideDown('slow');
+    $(".pg3bottomtext").slideDown('slow');
+    $(".pg3topimage").delay( 800 ).fadeIn('slow');
+  $(".img1").draggable();
 });
+
+ 
