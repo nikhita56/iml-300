551e90e53c604be0d0ee937302b4362fe4236e5a
Nikhita Rao <nikhitar@usc.edu>
2022-04-30 15:16:22 -0700

    personal touches

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+//        alert('You just scrolled!');
+    $('body').css('background', 'linear-gradient(0deg, rgba(2, 84, 94) 50%, rgba(232, 191, 255) 100%)');
+    
+    });
+});
